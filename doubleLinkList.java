3808b9bd6266bdde64e5c87d225b0761bbbbbabc
Philip-W <philipgwoods90@gmail.com>
2015-09-10 17:22:43 +0100

    Once more

---
+++ doubleLinkList.java
@@ -0,0 +1,35 @@
+
+
+
+public class doubleLinkList{
+
+  private Node head; //first element
+  private Node tail; //second element
+  private Node size;
+
+  public doubleLinkList(Object o){}// Constructor
+
+  public Node getHead(){ return head; }// returns the head Node.
+
+  public Node getTail() { return tail; }// returns the tail Node.
+
+  public int getSize() { return size; }// returns the number of Nodes in the list
+                                       // limits the size!
+
+  public addNode(Node n, int index){} //inserts a Node after the given index
+
+  public removeNode(int index){} //Removees a node at an index
+
+  public toString(){} //Standard toString method
+
+
+
+
+
+
+
+
+
+
+
+}
