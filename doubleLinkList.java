a67117c61ce60ccb2c285baa6d6ec4353ecdda7f
Philip-W <philipgwoods90@gmail.com>
2015-09-13 23:09:47 +0100

    re-fixed the addNode method, and added a removeNode method, should be complete, time to find something new to make

--- doubleLinkList.java
+++ doubleLinkList.java
@@ -6,15 +6,15 @@ public class doubleLinkList{
   private int size;
 
   public doubleLinkList(){
-    //testing an empty initialization, where the first node == null
+    //head node acts as a pointer to the rest of the list.
     Node h = new Node(null);
     head = h;
     tail = h;
 }
 
   public Node getHead(){
-    //returns head, only really used for iterating through the
-    //list since the head node is null.
+    //returns head, though this is null so is only a pointer to the first real
+    //value in the list.
     return head;
   }
 
@@ -23,18 +23,21 @@ public class doubleLinkList{
   public int getSize() { return size; }// returns the number of Nodes in the list
                                        // limits the size!
   public void addToTail(Object o){
+    //adds a Node to the end of the list, unlike the head the tail != null
+    //used to make exceptions in addNode easier and for general appending.
     Node n = new Node(o);
     tail.setNext(n);
     n.setPrev(tail);
     n.setNext(null);
     tail = n;
+    size++;
 
   }
 
 
   public void addNode(Object o, int index){
-    //Inserts a Node after the given index (might bug due to null head)
-    //if the index is > size, it will be appended to teh end of the list.
+    //Inserts a Node after the given index (might bug due to null head - fixed)
+    //if the index is > size, it will be appended to the end of the list.
 
     Node n = new Node(o);
     Node before = head;
@@ -44,12 +47,12 @@ public class doubleLinkList{
       throw new IndexOutOfBoundsException("Index "+ index + " is negative!");
     }
 
-    if (size == 0){
+    if (size == 0 || index >= size){
       addToTail(o);
       return;
     }
 
-    for (int i = 1; i <index && before.Next() != null; ++i){
+    for (int i = 0; i <index && before.Next() != null; ++i){
       before = before.Next();
     }
     after = before.Next();
@@ -61,7 +64,20 @@ public class doubleLinkList{
     size++;
   }
 
-  public void removeNode(int index){} //Removees a node at an index
+  public void removeNode(int index){
+    //head index = 0, head.Next() = 1.
+    if (index < 1 || index > size){
+      throw new IndexOutOfBoundsException();
+    }
+    Node current = head;
+    for (int i = 1; i < index; ++i){
+      current = current.Next();
+    }
+    Node nxt = current.Next().Next();
+    current.setNext(nxt);
+    nxt.setPrev(current);
+
+  }
 
   public String toString(){
     Node n = head.Next();
